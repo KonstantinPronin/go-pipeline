bc5e207e5ab507bbdbaaa16e1e73f9580c11ddc0
Konstantin Pronin <sayfer97@yandex.ru>
2020-02-17 23:54:45 +0300

    Initialized task

---
+++ signer/signer.go
@@ -0,0 +1,17 @@
+package main
+
+func SingleHash(in, out chan interface{}) {
+
+}
+
+func MultiHash(in, out chan interface{}) {
+
+}
+
+func CombineResults(in, out chan interface{}) {
+
+}
+
+func ExecutePipeline(jobs ...job) {
+
+}
