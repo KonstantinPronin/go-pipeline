e2826cbd77a511573bac26cb87a4e805e3c49b05
Konstantin Pronin <sayfer97@yandex.ru>
2020-02-18 18:54:30 +0300

    Added linear implementation

--- signer/signer.go
+++ signer/signer.go
@@ -1,17 +1,68 @@
 package main
 
-func SingleHash(in, out chan interface{}) {
+import (
+	"sort"
+	"strconv"
+)
 
+func SingleHash(in, out chan interface{}) {
+	for {
+		data, ok := <-in
+		if !ok {
+			break
+		}
+		out <- DataSignerCrc32(data.(string)) + "~" + DataSignerCrc32(DataSignerMd5(data.(string)))
+	}
+	close(out)
 }
 
 func MultiHash(in, out chan interface{}) {
+	for {
+		var result string
+		data, ok := <-in
 
+		if !ok {
+			break
+		}
+
+		for th := 0; th < 6; th++ {
+			result += DataSignerCrc32(strconv.Itoa(th) + data.(string))
+		}
+
+		out <- result
+	}
+	close(out)
 }
 
 func CombineResults(in, out chan interface{}) {
+	var result string
+	var storage []string
 
+	for {
+		data, ok := <-in
+
+		if !ok {
+			break
+		}
+
+		storage = append(storage, data.(string))
+	}
+
+	sort.Strings(storage)
+	for _, str := range storage {
+		if result != "" {
+			result += "_"
+		}
+		result += str
+	}
+	out <- result
+	close(out)
 }
 
 func ExecutePipeline(jobs ...job) {
-
+	in := make(chan interface{})
+	out := make(chan interface{})
+	for _, job := range jobs {
+		go job(in, out)
+	}
 }
