ecb78188afc559b93525c0f984d8d530fab649bc
Konstantin Pronin <sayfer97@yandex.ru>
2020-09-18 18:41:28 +0300

    Added implementation for go-pipeline

--- signer/signer.go
+++ signer/signer.go
@@ -3,35 +3,85 @@ package main
 import (
 	"sort"
 	"strconv"
+	"sync"
 )
 
 func SingleHash(in, out chan interface{}) {
+	wg := new(sync.WaitGroup)
+
 	for {
 		data, ok := <-in
 		if !ok {
 			break
 		}
-		out <- DataSignerCrc32(data.(string)) + "~" + DataSignerCrc32(DataSignerMd5(data.(string)))
+
+		val := strconv.Itoa(data.(int))
+		local := make(chan interface{})
+
+		wg.Add(1)
+		go func(local chan interface{}, val string) {
+			defer wg.Done()
+			local <- DataSignerCrc32(val)
+		}(local, val)
+
+		md5 := DataSignerMd5(val)
+
+		wg.Add(1)
+		go func(local, out chan interface{}, val string) {
+			defer wg.Done()
+			val = DataSignerCrc32(val)
+			out <- (<-local).(string) + "~" + val
+		}(local, out, md5)
 	}
-	close(out)
+
+	wg.Wait()
 }
 
 func MultiHash(in, out chan interface{}) {
-	for {
-		var result string
-		data, ok := <-in
+	wg := new(sync.WaitGroup)
 
+	for {
+		data, ok := <-in
 		if !ok {
 			break
 		}
 
-		for th := 0; th < 6; th++ {
-			result += DataSignerCrc32(strconv.Itoa(th) + data.(string))
-		}
+		wg.Add(1)
+		go func(data interface{}) {
+			defer wg.Done()
+			channels := []chan interface{}{
+				make(chan interface{}),
+				make(chan interface{}),
+				make(chan interface{}),
+				make(chan interface{}),
+				make(chan interface{}),
+				make(chan interface{}),
+			}
 
-		out <- result
+			for th, ch := range channels {
+				wg.Add(1)
+				go func(th int, local chan interface{}) {
+					defer wg.Done()
+					res := DataSignerCrc32(strconv.Itoa(th) + data.(string))
+					local <- res
+				}(th, ch)
+			}
+
+			wg.Add(1)
+			go func(channels []chan interface{}, out chan interface{}) {
+				var result string
+				defer wg.Done()
+
+				for _, ch := range channels {
+					result += (<-ch).(string)
+				}
+
+				out <- result
+			}(channels, out)
+		}(data)
 	}
-	close(out)
+
+	wg.Wait()
 }
 
 func CombineResults(in, out chan interface{}) {
@@ -40,7 +90,6 @@ func CombineResults(in, out chan interface{}) {
 
 	for {
 		data, ok := <-in
-
 		if !ok {
 			break
 		}
@@ -49,20 +98,36 @@ func CombineResults(in, out chan interface{}) {
 	}
 
 	sort.Strings(storage)
+
 	for _, str := range storage {
 		if result != "" {
 			result += "_"
 		}
 		result += str
 	}
+
 	out <- result
-	close(out)
 }
 
 func ExecutePipeline(jobs ...job) {
+	wg := new(sync.WaitGroup)
 	in := make(chan interface{})
-	out := make(chan interface{})
-	for _, job := range jobs {
-		go job(in, out)
+
+	for _, j := range jobs {
+		out := make(chan interface{})
+
+		wg.Add(1)
+
+		go func(j job, in, out chan interface{}) {
+			defer func() {
+				close(out)
+				wg.Done()
+			}()
+			j(in, out)
+		}(j, in, out)
+
+		in = out
 	}
+
+	wg.Wait()
 }
